e869476616817a7f4d92cbcd16e6fcf84dca3dbb
39334 <39334@172.21.119.87>
2023-03-08 16:42:33 +0100

    Completato il lab

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -1,6 +1,10 @@
 package it.polito.tdp.Lab01;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.Lab01.model.Parole;
@@ -25,21 +29,69 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private Button btncancella;
 
     @FXML
     private TextField txtParola;
+    
+    @FXML
+    private TextField txtParolaCancella;
+    
+    @FXML
+    private TextArea txtPerformance;
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML 
+    Parole ppp = new Parole();
+    
 
     @FXML
-    void doInsert(ActionEvent event) {
+    void doCancel(ActionEvent event) {
+    	long tic = System.nanoTime();
 
+    	ppp.cancelParola(this.txtParolaCancella.getText());
+    	
+    	this.txtResult.setText("");
+    	
+    	for(String s : ppp.getElenco()) {
+    		this.txtResult.appendText(s + "\n");
+    	}
+    	long toc = System.nanoTime();
+    	
+    	this.txtPerformance.appendText("Tempo di Cancella: " + (toc-tic) + "\n");
+    }
+    
+    @FXML
+    void doInsert(ActionEvent event) {   
+    	
+    	long tic = System.nanoTime();
+    	
+    	ppp.addParola(this.txtParola.getText());
+    	
+    	this.txtResult.setText("");
+    	    	
+    	for(String s : ppp.getElenco()) {
+    		this.txtResult.appendText(s + "\n");
+    	}
+    	
+    	long toc = System.nanoTime();
+    	
+    	this.txtPerformance.appendText("Tempo di Inserisci: " + (toc-tic) + "\n");
+    	
     }
 
     @FXML
     void doReset(ActionEvent event) {
-
+    	long tic = System.nanoTime();
+    	ppp.reset();
+    	this.txtResult.setText(null);
+    	long toc = System.nanoTime();
+    	
+    	this.txtPerformance.appendText("Tempo di Reset: " + (toc-tic) + "\n");
     }
 
     @FXML
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,35 @@
 package it.polito.tdp.Lab01.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Parole {
+	
+    ArrayList<String> elencoParole = new ArrayList<String>();
 		
 	public Parole() {
 		//TODO
 	}
 	
+	public void cancelParola(String p) {
+		elencoParole.remove(p);
+    	Collections.sort(elencoParole);
+	}
+	
 	public void addParola(String p) {
-		//TODO
+    	elencoParole.add(p.toLowerCase());
+    	Collections.sort(elencoParole);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		return elencoParole;
 	}
 	
 	public void reset() {
-		// TODO
+		for(String s : elencoParole){
+			elencoParole.remove(s);
+		}
 	}
 
 }
